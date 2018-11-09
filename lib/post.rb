3a8e05a43c1a41a864a394d068dcea82653e8f34
Jacob Feldman <jacobgfeld@gmail.com>
2018-11-09 20:18:14 +0000

    automatically backed up by learn

---
+++ lib/post.rb
@@ -0,0 +1,14 @@
+class Song
+  attr_accessor :name, :artist
+  @@songs = []
+  
+  def initialize(name)
+    @name = name
+    @@songs << self
+  end
+  
+  def self.all
+    @@songs
+  end
+  
+end
